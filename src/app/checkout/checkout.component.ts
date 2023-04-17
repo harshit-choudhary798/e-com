894aabf3dff8477cb16f11accba377a388a1f22c
Harshit Choudhary <harshit1840077@gmail.com>
2023-04-17 11:23:53 +0530

    sending custome message to admin regarding order

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MainService } from '../main.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -7,13 +8,61 @@ import { MainService } from '../main.service';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+products:any
+form=new FormGroup({
+  name: new FormControl('',Validators.required),
+  address: new FormControl('', Validators.required),
+  phone: new FormControl('', Validators.required),
+})
+get name() {
+  return this.form.get('name');
+}
+get address() {
+  return this.form.get('address');
+}
+get phone() {
+  return this.form.get('phone');
+}
 
   constructor(private services:MainService) { }
+  message:any
+
+
   submit(){
+    console.log(this.form.value);
+//    this.message = `Name: ${this.name?.value} \n Address: ${this.address?.value} \n Contact No.: ${this.phone?.value}\n`;
+    this.message = `
+    Name: ${this.name?.value}  Address: ${this.address?.value} \n Contact No.: ${this.phone?.value}
+    The ordered products are:
+    Product
     
-    this.services.sendMessage('TOKEN_ID','CHAT_ID','finally').subscribe({})
+   -----------------------------------
+    ${this.content} \n
+    Total Amount: 
+    ${this.getTotalAmount()}`;
+    console.log(this.message)
+    this.services.sendMessage('TOKEN','CHAT_ID',this.message).subscribe({})
   }
-  ngOnInit(): void {
+
+  titles:any
+  price:any
+  content:any
+  ngOnInit() {
+    this.products=this.services.cartProducts
+    this.products.value
+    this.products.map((product:any)=>{
+      this.content += ` ${product.title} : price $${product.price}`;
+      
+    })
+    
+   
+  }
+  getTotalAmount() {
+    let total = 0;
+    for (let i = 0; i < this.products.length; i++) {
+      total += this.products[i].price;
+    }
+    return total.toFixed(2);
   }
 
 }
